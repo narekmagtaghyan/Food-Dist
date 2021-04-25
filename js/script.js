79747b66c00b4c8d35c5b0e70042814802e07541
Narek Magtaghyan <narekmagtaghyan@gmail.com>
2021-04-25 23:23:53 +0400

    Шаблонизация карт для меню

--- js/script.js
+++ js/script.js
@@ -150,4 +150,71 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 
     window.addEventListener("scroll", showModalByScroll);
+
+    /* === МЕНЮ СЕРВИСА === */
+    // Шаблонизация карт для меню
+    class MenuCard {
+        // Собирает все нужные данные
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        // Переводит с доллара на гривны
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        // Отображает карту на страницу
+        render() {
+            const element = document.createElement("div");
+            element.innerHTML = `                
+            <div class="menu__item">
+                <img src="${this.src}" alt="${this.alt}">
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            </div>`;
+
+            this.parent.append(element);
+        }
+    }
+
+    // Создание карт
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        14,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        21,
+        ".menu .container"
+    ).render();
 });
