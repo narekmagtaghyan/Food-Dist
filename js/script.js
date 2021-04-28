ac13b70f0a496dbd463d5bc10fa629e4641219dd
Narek Magtaghyan <narekmagtaghyan@gmail.com>
2021-04-28 22:21:21 +0400

    Улучшена функция шаблонизации карт

--- js/script.js
+++ js/script.js
@@ -155,12 +155,13 @@ window.addEventListener("DOMContentLoaded", () => {
     // Шаблонизация карт для меню
     class MenuCard {
         // Собирает все нужные данные
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
             this.changeToUAH();
@@ -171,11 +172,17 @@ window.addEventListener("DOMContentLoaded", () => {
             this.price = this.price * this.transfer;
         }
 
-        // Отображает карту на страницу
+        // Отображает карты меню на страницу
         render() {
             const element = document.createElement("div");
+            if (this.classes.indexOf('menu__item') === -1) {
+                this.element = "menu__item";
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
             element.innerHTML = `                
-            <div class="menu__item">
                 <img src="${this.src}" alt="${this.alt}">
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">${this.descr}</div>
@@ -184,7 +191,7 @@ window.addEventListener("DOMContentLoaded", () => {
                     <div class="menu__item-cost">Цена:</div>
                     <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
-            </div>`;
+            `;
 
             this.parent.append(element);
         }
@@ -197,7 +204,7 @@ window.addEventListener("DOMContentLoaded", () => {
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
-        ".menu .container"
+        ".menu .container",
     ).render();
 
     new MenuCard(
@@ -206,7 +213,7 @@ window.addEventListener("DOMContentLoaded", () => {
         'Меню "Постное"',
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         14,
-        ".menu .container"
+        ".menu .container",
     ).render();
 
     new MenuCard(
@@ -215,6 +222,7 @@ window.addEventListener("DOMContentLoaded", () => {
         'Меню “Премиум”',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         21,
-        ".menu .container"
+        ".menu .container",
+        "menu__item"
     ).render();
 });
